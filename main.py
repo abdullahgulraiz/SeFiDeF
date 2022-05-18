87a685bad3a687cde28627c7357be1841a97af1d
Gulraiz <abdullah.gulraiz.ext@siemens.com>
2022-05-18 19:18:54 +0200

    Added env vars for ds path

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
+from pathlib import Path
+
 import runcases
 
 
 if __name__ == '__main__':
-    sefila_dynamic_ds_path = "/home/abdullah/LRZ Sync+Share/TUM BMC/Master Thesis/Data/labeled-dataset-markus-new-with-tools.json"
-    sefila_static_ds_path = "/home/abdullah/LRZ Sync+Share/TUM BMC/Master Thesis/Data/labeled-dataset-static-tools.json"
-    pkl_static_ds_path = "/home/abdullah/LRZ Sync+Share/TUM BMC/Master Thesis/Data/sefidef-corpus-static-tools-scraped.pkl"
-    anchore_trivy_descriptions_pkl = "/home/abdullah/LRZ Sync+Share/TUM BMC/Master Thesis/Data/anchore_trivy_descriptions_scraped.pkl"
-    pkl_static_ds_path_2 = "/home/abdullah/LRZ Sync+Share/TUM BMC/Master Thesis/Data/anchore_trivy_cve_id_package_path_url_scraped.pkl"
+    base_path = os.environ.get("DATA_BASE_PATH")
+    if not base_path:
+        raise RuntimeError("Please define the base path for data files as the environment variable DATA_BASE_PATH.")
+    base_path = Path(base_path)
+    sefila_dynamic_ds_path = base_path / "labeled-dataset-markus-new-with-tools.json"
+    sefila_static_ds_path = base_path / "labeled-dataset-static-tools.json"
+    pkl_static_ds_path = base_path / "sefidef-corpus-static-tools-scraped.pkl"
+    anchore_trivy_descriptions_pkl = base_path / "anchore_trivy_descriptions_scraped.pkl"
+    pkl_static_ds_path_2 = base_path / "anchore_trivy_cve_id_package_path_url_scraped.pkl"
     run_cases = [
         # *runcases.gensim_lsi_zap_arachni_runcases(ds_path=sefila_dynamic_ds_path),
         # *sbert_zap_arachni_runcases(ds_path=sefila_ds_path)
@@ -15,8 +22,8 @@ if __name__ == '__main__':
         # *runcases.sbert_trivy_anchore_runcases_1(ds_path=pkl_static_ds_path),
         # *runcases.sbert_trivy_anchore_runcases_2(ds_path=pkl_static_ds_path_2),
         *runcases.equality_comparison_static_tools(
-            unique_ds_path=sefila_static_ds_path,
-            target_ds_path=anchore_trivy_descriptions_pkl
+            unique_ds_path=str(sefila_static_ds_path),
+            target_ds_path=str(anchore_trivy_descriptions_pkl)
         )
     ]
     for run_case in run_cases:
